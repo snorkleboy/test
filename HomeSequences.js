e02e415d1f7867bd4f1ca866c2209bd63fb5789c
Artem Kharshan <timkharshan@hotmail.com>
2018-02-19 12:29:49 -0800

    reading sample data, splitting into n,k, and price array

---
+++ HomeSequences.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path'),
+filePath = path.resolve(process.cwd(), process.argv[2]);
+
+
+fs.readFile(filePath, 'utf8', function (err, contents) {
+    if (err){
+        console.log(`error opening file: ${filePath}`,err);
+    }else{
+        console.log(contents);
+        const data = contents.split(/[\n]/);
+        nk = data[0].split(' ');
+        priceArray = data[1].split(' ');
+        console.log('n',nk[0],'k',nk[1],'prices',priceArray);
+
+
+    }
+    
+});
